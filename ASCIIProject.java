3023e0da086d4aef6001aff82452508a7c45553b
squidwardlyPossessed <ja2211@hsutx.edu>
2024-03-19 13:10:55 -0500

    my first commit vallah

---
+++ ASCIIProject.java
@@ -0,0 +1,59 @@
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+public class ASCIIProject{
+    private static String filename;
+    private static String density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+
+    public static void main(String[] args) throws IOException{
+        
+        filename = "/Users/javidanaghayev/Downloads/white.jpg";
+        File file = new File(filename);
+        BufferedImage image = ImageIO.read(file);
+;
+        int x = 50; 
+        int y = 50; 
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        System.out.println("Image width: " + width);
+        System.out.println("Image height: "+ height);
+        System.out.println("Aspect ratio: " + getAspectRatio(width, height));
+        RGBValues(x, y);
+
+    }
+
+    public static String getAspectRatio(int width, int height) {
+        int gcd = gcd(width, height);
+        int aspectWidth = width / gcd;
+        int aspectHeight = height / gcd;
+        return aspectWidth + ":" + aspectHeight;
+    }
+
+    public static int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void RGBValues(int x, int y) throws IOException{
+        File file = new File(filename);
+        BufferedImage image = ImageIO.read(file);
+
+        int rgb = image.getRGB(x, y);
+            
+        int red = (rgb >> 16) & 0xFF;
+        int green = (rgb >> 8) & 0xFF;
+        int blue = rgb & 0xFF;
+
+        System.out.println("RGB values at (" + x + ", " + y + "):");
+        System.out.println("Red: " + red);
+        System.out.println("Green: " + green);
+        System.out.println("Blue: " + blue);
+    }
+}
