aaf422e68a3236ccc505c7a07afc4a1440d8a7c3
squidwardlyPossessed <ja2211@hsutx.edu>
2024-06-04 14:35:03 +0400

    successfully wrote to a new file under a new name, instead of modifying the file itself

--- ASCIIProject.java
+++ ASCIIProject.java
@@ -4,62 +4,57 @@ import java.io.IOException;
 import javax.imageio.ImageIO;
 
 public class ASCIIProject{
-    private static String filename;
-    private static String density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
+   private static String filename;
+   private static String density = "Ñ@#W$9876543210?!abc;:+=-,._ ";
 
-    public static void main(String[] args) throws IOException{
-        
-        filename = "/Users/javidanaghayev/Downloads/flashbang.jpeg";
-        File file = new File(filename);
-        BufferedImage image = ImageIO.read(file);
-;
-        int x = 50; //finds the RGB value at x axis
-        int y = 50; //finds the RGB value at y axis
+   public static void main(String[] args) throws IOException{
+      filename = args[0];       
+      File file = new File(filename);
+      BufferedImage image = ImageIO.read(file);
 
-        int width = image.getWidth();
-        int height = image.getHeight();
-        int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+      int width = image.getWidth();
+      int height = image.getHeight();
+      int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
 
-        // System.out.println("Image width: " + width);
-        // System.out.println("Image height: "+ height);
-        // System.out.println("Aspect ratio: " + getAspectRatio(width, height));
+      // System.out.println("Image width: " + width);
+      // System.out.println("Image height: "+ height);
+      // System.out.println("Aspect ratio: " + getAspectRatio(width, height));
 
-        for(int i = 0; i < pixels.length; i++){
+      for(int i = 0; i < pixels.length; i++){
 
-           int p = pixels[i];
+         int p = pixels[i];
 
-           int a = ( p >> 24) & 0xFF;    
-           int red = (p >> 16) & 0xFF;
-           int green = (p >> 8) & 0xFF;
-           int blue = p & 0xFF;
+         int a = ( p >> 24) & 0xFF;    
+         int red = (p >> 16) & 0xFF;
+         int green = (p >> 8) & 0xFF;
+         int blue = p & 0xFF;
 
-           int avg = (red + green + blue) / 3;
-           p = (a << 24) | (avg << 16) | (avg << 8) | avg;
-           pixels[i] = p;
-        }
-        image.setRGB(0, 0, width, height, pixels, 0, width);
-        try {
-           file = new File(
-                 "/Users/javidanaghayev/Downloads/greyflashbang.jpeg");
-           ImageIO.write(image, "jpg", file);
-        } catch (IOException e) {
-           System.out.println(e);
-        }
-    }
+         int avg = (red + green + blue) / 3;
+         p = (a << 24) | (avg << 16) | (avg << 8) | avg;
+         pixels[i] = p;
+      }
+      image.setRGB(0, 0, width, height, pixels, 0, width);
+      try {
+         file = new File(filename.replace(".jpg", "_grayscale.jpg"));
+         ImageIO.write(image, "jpg", file);
+      } catch (IOException e) {
+         System.out.println(e);
+      }
+   }
 
-    public static String getAspectRatio(int width, int height) {
-        int gcd = gcd(width, height);
-        int aspectWidth = width / gcd;
-        int aspectHeight = height / gcd;
-        return aspectWidth + ":" + aspectHeight;
-    }
+   public static String getAspectRatio(int width, int height) {
+      int gcd = gcd(width, height);
+      int aspectWidth = width / gcd;
+      int aspectHeight = height / gcd;
+      return aspectWidth + ":" + aspectHeight;
+   }
 
-    public static int gcd(int a, int b) {
-        while (b > 0) {
-            int temp = b;
-            b = a % b;
-            a = temp;
-        }
-        return a;
-    }
+   public static int gcd(int a, int b) {
+      while (b > 0) {
+         int temp = b;
+         b = a % b;
+         a = temp;
+      }
+      return a;
+   }
 }
