2f870426d112fc510097e6313f23859367cea3ce
Javidan <ja2211@hsutx.edu>
2024-08-06 17:15:33 +0400

    first commit...again (for some reason .git disappeared)

---
+++ ASCIIProject.java
@@ -0,0 +1,143 @@
+//salam
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.util.Scanner;
+
+public class ASCIIProject{
+    private static String filename;
+
+    public static void main(String[] args) throws IOException{
+        Scanner input = new Scanner(System.in);
+        int size;
+        System.out.print("⠄⢀⣀⣤⣴⣶⣶⣤⣄⡀⠄⠄⣀⣤⣤⣤⣤⡀⠄⠄⠄⠄⠀⠀⠀⠀⠀⠀                        \n"
+                +"⣴⣏⣹⣿⠿⠿⠿⠿⢿⣿⣄⢿⣿⣿⣿⣿⣿⣋⣷⡄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀ ⢠⣠⣾⣷⣿⣿⣿⣷⣄⠄⠀         \n"
+                +"⣿⢟⣩⣶⣾⣿⣿⣿⣶⣮⣭⡂⢛⣭⣭⣭⣭⣭⣍⣛⣂⡀⠄⠄⠄⠄⠄⠄⠄⠄        ⣀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣷⣦⢅⠀        \n"
+                +"⣿⣿⣿⣿⡿⢟⣫⣭⣷⣶⣾⣭⣼⡻⢛⣛⣭⣭⣶⣶⣬⣭⣅⡀⠄⠄⠄⠄⠄⠄⠀⠀⠀⠀⠀⠀ ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄⡀⠀      \n"
+                +"⣿⡿⢏⣵⣾⣿⣿⣿⡿⢉⡉⠙⢿⣇⢻⣿⣿⣿⣿⡟⠉⠉⢻⡷⠄⠄⠄⠄⠄⠄ ⠀⠀⠀⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡗       \n"
+                +"⣿⣷⣾⣍⣛⢿⣿⣿⣿⣤⣁⣤⣿⢏⠸⣿⣿⣿⣿⣷⣬⣥⣾⠁⣿⣿⣷⠄⠄⠄⠀⠀   ⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀   \n"
+                +"⣿⣿⣿⣿⣭⣕⣒⠿⠭⠭⠭⡷⢖⣫⣶⣶⣬⣭⣭⣭⣭⣥⡶⢣⣿⣿⣿⠄⠄⠄⠀⠀⠀⠀⠀⠘⢿⣿⠁⣩⣿⣿⣿⠿⣿⡿⢿⣿⣿⣿⠛⣿⡟⠀⠀⠀⠀⠀  \n"
+                +"⣿⣿⣿⣿⣿⣿⣿⡿⣟⣛⣭⣾⣿⣿⣿⣝⡛⣿⢟⣛⣛⣁⣀⣸⣿⣿⣿⣀⣀⣀⠀⠀⠀⠀   ⢷⣾⣿⣋⡡⠤⣀⣷⣄⠠⠤⣉⣿⣷⣽⠀⠀⠀⠀⠀⠀  \n"
+                +"⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀   ⠈⣿⣿⣿⣿⣿⣿⣿⣿⡻⣾⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀  \n"
+                +"⣿⡿⢛⣛⣛⣛⣙⣛⠿⠿⣿⣿⣿⣿⣿⣿⣿⣿⣬⣭⣭⠽⣛⢻⣿⣿⣿⠛⠛⠛⠀⠀⠀⠀⠀⠀⠀ ⠙⣿⣟⢋⣰⣯⠉⠉⣿⢄⠉⢻⡟⠀⠀⠀⠀⠀⠀⠀  \n"
+                +"⣿⢰⣿⣿⣿⣿⣟⣛⣛⣶⠶⠶⠶⣦⣭⣭⣭⣭⣶⡶⠶⣾⠟⢸⣿⣿⣿⠄⠄⠄⠀⠀⠀⠀⠀⠀   ⠹⣿⢿⣷⣶⠤⠔⣶⣶⠿⢾⣧⠀⠀⠀⠀⠀⠀⠀  \n"
+                +"⡻⢮⣭⣭⣭⣭⣉⣛⣛⡻⠿⠿⠷⠶⠶⠶⠶⣶⣶⣾⣿⠟⢣⣬⣛⡻⢱⣇⠄⠄   ⢀⡀⠠⠀⠂⠀⠀⣧⡚⢿⣿⡶⢶⡿⠟⣠⣿⣿⠀⠀⠀⠀⠄⣀⡀  \n"
+                +"⣿⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⠶⠒⠄⠄⠄⢸⣿⢟⣫⡥⡆⠄⠄⠒⠒⠋⠁⠀⠀⠀⠀⠀⠀⢿⣷⣄⡀⠀⠀⠀⣈⣴⣿⣿⠀⠀⠀⠀⠀⠀⠀⠉⠒\n"
+                +"⢭⣭⣝⣛⣛⣛⣛⣛⣛⣛⣿⣿⡿⢛⣋⡉⠁⠄⠄⠄⠄⠄⢸⣿⢸⣿⣧⡅⠄⠄⠀⠀⠀⠀⠀⠀⠀   ⢸⣿⣿⡿⠒⠐⠺⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀\n"
+                +"⣶⣶⣶⣭⣭⣭⣭⣭⣭⣵⣶⣶⣶⣿⣿⣿⣦⡀⠄⠄⠄⠄⠈⠡⣿⣿⡯⠁⠄⠄          ⢸⢿⣋⣀⡄⠠⣢⣀⣩⣛⠇       \n\n");
+        System.out.print("+----------------------------------------------------------+\n"
+                        +"| Hail and blessings upon thy soul, for gracing my program |\n"
+                        +"| with thy presence! Come hither, and I shall show thee    |\n"
+                        +"| that which thou must needs know ere thou dost proceed... |\n"
+                        +"+----------------------------------------------------------+\n\n");
+        System.out.print("------------------------------------------------------------\n"
+                        +"1. This program will turn your photos into ASCII art, BUT   \n"
+                        +"it is still in the works, so I recommend you                \n"
+                        +"to use photos that you are not afraid to lose, lest they    \n"
+                        +"become corrupted.                                         \n\n"
+                        +"2. NEVER USE THE ORIGINAL PHOTO, SINCE IRREVERSIBLE         \n"
+                        +"CHANGES COULD BE MADE TO THEM                             \n\n"
+                        +"3. Chunk size refers to the amount of pixels compressing,   \n"
+                        +"for example 3 means that 3x3 pixels are read as one.        \n"
+                        +"So the bigger the number, the smaller the \'photo\'.        \n"
+                        +"Typing 1 as the chunk size is not recommended, since it's   \n"
+                        +"going to print the original picture, which will result in   \n"
+                        +"lag and a big text file.                                  \n\n"
+                        +"4. Currently, the saved .txt file will be saved in the dir  \n"
+                        +"this program was opened. Custom directory selection not     \n"
+                        +"available yet (maybe some other time)                     \n\n"
+                        +"5. When you open the created .txt file, make sure to        \n"
+                        +"reduce font size (ctrl - or cmd -)                          \n"
+                        +"6. It is highly recommended to use a square font, since it  \n"
+                        +"will make the image look better. "
+                        +"------------------------------------------------------------\n\n");
+        System.out.print("Are you ready to proceed? [y/n]: ");
+
+        String confirmation = input.nextLine();
+        while(!confirmation.equalsIgnoreCase("Y")){
+            if(confirmation.equalsIgnoreCase("Y")){
+                System.out.println("proceeding...");
+            }else if(confirmation.equalsIgnoreCase("N") || confirmation.equalsIgnoreCase("NO")){
+                System.out.println("Leaving program...");
+                System.exit(1);
+            }else{
+                System.out.print("Are you ready to proceed? [y/n]: ");
+                confirmation = input.nextLine();
+            }
+        }
+
+        System.out.print("PATH to your image: ");
+        filename = input.nextLine();
+
+        File file = new File(filename);
+        BufferedImage image = ImageIO.read(file);
+
+        System.out.print("Select chunk size (bigger chunk means smaller, blurrier picture) (ex. 3 or 4):  ");
+        size = input.nextInt();
+
+        int width = image.getWidth();
+        int height = image.getHeight();
+        ImageCulling imageCulling = new ImageCulling(width, height, size ); // Adjust width, height, and chunk size
+        int[] pixels = image.getRGB(0, 0, width, height, null, 0, width);
+
+        System.out.println("Image width: " + width);
+        System.out.println("Image height: "+ height);
+        System.out.println("Aspect ratio: " + getAspectRatio(width, height));
+
+        int count = 0;
+        for(int i = 0; i < pixels.length; i++){
+            int p = pixels[i];
+
+            int a = ( p >> 24) & 0xFF; //isolate pixel density value
+            int red = (p >> 16) & 0xFF; //isolate red pixel value
+            int green = (p >> 8) & 0xFF; //isolate green pixel value
+            int blue = p & 0xFF; //isolate blue pixel value
+
+            int avg = (red + green + blue) / 3;
+            p = (a << 24) | (avg << 16) | (avg << 8) | avg;
+            pixels[i] = avg;  //was pixels[i] = p
+        }
+
+        image.setRGB(0, 0, width, height, pixels, 0, width);
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("\nName your file (omit extensions): ");
+        filename = in.nextLine();
+
+        if (filename.isEmpty()) {
+            System.out.println("Invalid filename. Exiting.");
+            return;
+        }
+
+        File outputFile = new File( filename + ".txt");
+        if (outputFile.exists()) {
+            System.out.println("File already exists. Exiting.");
+            return;
+        }
+
+        // System.out.println("File saved successfully at " + file.getAbsolutePath());
+
+        imageCulling.processImage(pixels); //does actually process and place new average density into array
+        imageCulling.printNewArray(filename);      //does actualy print new array
+        // imageCulling.printArray(pixels);
+    }
+
+    public static String getAspectRatio(int width, int height) {
+        int gcd = gcd(width, height);
+        int aspectWidth = width / gcd;
+        int aspectHeight = height / gcd;
+        return aspectWidth + ":" + aspectHeight;
+    }
+
+    public static int gcd(int a, int b) {
+        while (b > 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
---
+++ ImageCulling.java
@@ -0,0 +1,75 @@
+//salam
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ImageCulling {
+    private int WIDTH;
+    private int HEIGHT;
+    private int CHUNK_SIZE;
+    private int CHUNK;
+    private int[][] new_array;
+    private static String density = "Ñ@#W$9876543210?!abc;:+=-,._ "; // 28 characters
+
+    public ImageCulling(int width, int height, int chunkSize) {
+        this.WIDTH = width;
+        this.HEIGHT = height;
+        this.CHUNK_SIZE = chunkSize;
+        this.CHUNK = CHUNK_SIZE * CHUNK_SIZE;
+        this.new_array = new int[HEIGHT / CHUNK_SIZE + 1][WIDTH / CHUNK_SIZE + 1];
+    }
+
+    public void processImage(int[] arr) {
+        int new_x = 0;
+        int new_y = 0;
+
+        for (int y = 0; y < HEIGHT; y++) {
+            new_x = 0;
+            if (y != 0 && y % CHUNK_SIZE == 0) {
+                new_y++;
+            }
+            for (int x = 0; x < WIDTH; x++) {
+                if (x != 0 && x % CHUNK_SIZE == 0) {
+                    new_x++;
+                }
+                new_array[new_y][new_x] += arr[WIDTH * y + x];
+            }
+        }
+    }
+
+    public void printArray(int[] arr) {
+        int count = 0;
+        System.out.print("This is the original array \n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("count: " + count + " array density: " + arr[i]);
+            if (count % WIDTH == 0) System.out.print("\n");
+        }
+        System.out.print("\n\n\n");
+    }
+
+    public void printNewArray(String filename) throws IOException{
+        PrintWriter writer = new PrintWriter(filename+".txt", "UTF-8");
+        BufferedWriter bufferedWriter = new BufferedWriter(writer);
+        System.out.print("This is the compressed version of the art \n");
+        for (int i = 0; i < HEIGHT / CHUNK_SIZE; i++) {
+            for (int j = 0; j < WIDTH / CHUNK_SIZE; j++) {
+                System.out.print(denser(new_array[i][j]/CHUNK)); // /CHUNK)); why do we have divided by CHUNK
+                writer.print(denser(new_array[i][j]/CHUNK)); //CHUNK is important
+            }
+            System.out.print("\n");
+            writer.print("\n");
+        }
+        writer.close();
+    }
+
+    public static char denser(int avg){
+        int count = 28;
+        for(int bit = 0; bit < 255; bit++){
+            if(bit != 0 && bit % 9 == 0) density.charAt(count--);
+            if(bit == avg) return density.charAt(count);
+        }
+        return ' ';
+    }
+}
+
